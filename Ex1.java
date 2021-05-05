7d2db59df4c0499e28dec56b7e4bc1b3b408f8f0
navkc11 <63931144+navkc11@users.noreply.github.com>
2021-05-05 17:15:19 +0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Ex1 {
     public static void main() {
         int[] arr1 = {1,2,3,4,5,6,7};
@@ -13,9 +15,8 @@ public class Ex1 {
 
                 System.out.println(Math.max(arr1[i], arr2[i]));
             }catch (ArrayIndexOutOfBoundsException e){
-                for (i = 0; i < arr2.length; i++) {
-                    System.out.println(arr2[i]);
-                }
+                System.out.println(Arrays.toString(arr2));
+                break;
             }
         }
     }
